23d542e062d5b4a3144d4757fce77a7a1f5ce2b5
dimagarn <56264897+dimagarn@users.noreply.github.com>
2023-10-13 01:16:37 +0500

    Task 1

---
+++ task1/main.py
@@ -0,0 +1,27 @@
+filename = "text_1_var_4"
+with open(filename) as file:
+    lines = file.readlines()
+
+word_stat = dict()
+
+for line in lines:
+    row = (line.strip()
+           .replace("!", " ")
+           .replace("?", " ")
+           .replace(",", " ")
+           .replace(".", " ")
+           .strip())
+
+    words = row.split(" ")
+
+    for word in words:
+        if word in word_stat:
+            word_stat[word] += 1
+        else:
+            word_stat[word] = 1
+
+word_stat = dict(sorted(word_stat.items(), reverse=True, key=lambda item: item[1]))
+
+with open("text_1_var_4.txt", "w") as result:
+    for key, value in word_stat.items():
+        result.write(key + ":" + str(value) + "\n")
